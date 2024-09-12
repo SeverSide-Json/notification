2766a81ac5d58665094b02392459e00fcba4509a
SeverSide-Json <dangminhtienfun1994@gmail.com>
2024-09-13 00:28:29 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,9 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('ladipage-pwa-cache').then(function(cache) {
       return cache.addAll([
-        '/',
-        'https://preview.ldpdemo.com/test-code',
-        'https://severside-json.github.io/notification/notifications.json',
-        'https://severside-json.github.io/notification/icon-192x192.png',
+        '/test-code', // Đường dẫn chính xác đến trang gốc của bạn
+        'https://raw.githubusercontent.com/username/repo/main/manifest.json', // Đường dẫn đến manifest
+        'https://severside-json.github.io/notification/icon-192x192.png', // Icon
       ]);
     })
   );
@@ -17,16 +16,5 @@ self.addEventListener('fetch', function(event) {
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
     })
-  );
-});
-
-self.addEventListener('push', function(event) {
-  const data = event.data ? event.data.json() : { title: 'Default title', body: 'Default body' };
-  const options = {
-    body: data.body,
-    icon: 'https://raw.githubusercontent.com/username/repo/main/icons/icon-192x192.png',
-  };
-  event.waitUntil(
-    self.registration.showNotification(data.title, options)
   );
 });
