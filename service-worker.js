552e70b00061a5160f65cd6d38ef8e141e588ff0
SeverSide-Json <dangminhtienfun1994@gmail.com>
2024-09-13 00:55:19 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('ladipage-pwa-cache').then(function(cache) {
       return cache.addAll([
-        '/index-github.html', // Đường dẫn chính xác đến trang gốc của bạn
+        'https://severside-json.github.io/notification/', // Đường dẫn chính xác đến trang gốc của bạn
         'https://severside-json.github.io/notification/notifications.json', // Đường dẫn đến manifest
         'https://severside-json.github.io/notification/icon-192x192.png', // Icon
       ]);
